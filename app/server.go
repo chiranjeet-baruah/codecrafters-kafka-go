f91412a14045b9e448106b95f68674e9ed284c7c
chiranjeet.baruah <chiranjeet.baruah@truecaller.com>
2025-02-19 11:49:01 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -13,19 +13,22 @@ type ApiVersionsRequest struct {
 	RequestApiKey     int16
 	RequestApiVersion int16
 	CorrelationID     int32
-	APIVersion        int16
+}
+
+type ApiKeyVersion struct {
+	ApiKey     int16
+	MinVersion int16
+	MaxVersion int16
 }
 
 type ApiVersionsResponse struct {
-	MessageSize   int32
-	CorrelationID int32
-	ErrorCode     int16
-	APIVersion    int16
+	MessageSize    int32
+	CorrelationID  int32
+	ErrorCode      uint16
+	ApiKeyCount    int8
+	ApiKeys        []ApiKeyVersion
+	ThrottleTimeMs int32
 }
-
-const (
-	MAX_API_VERSION = 18
-)
 
 func main() {
 	fmt.Println("Starting Kafka server...")
@@ -50,7 +53,7 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	buf := make([]byte, 1024)
+	buf := make([]byte, 4096)
 	n, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("Error reading:", err)
@@ -61,33 +64,100 @@ func handleConnection(conn net.Conn) {
 	reader := bytes.NewReader(data)
 
 	var request ApiVersionsRequest
-	binary.Read(reader, binary.BigEndian, &request.MessageSize)
-	binary.Read(reader, binary.BigEndian, &request.RequestApiKey)
-	binary.Read(reader, binary.BigEndian, &request.RequestApiVersion)
-	binary.Read(reader, binary.BigEndian, &request.CorrelationID)
-	binary.Read(reader, binary.BigEndian, &request.APIVersion)
-
-	// Prepare response
-	response := ApiVersionsResponse{
-		MessageSize:   10,
-		CorrelationID: request.CorrelationID,
+	// Read basic request fields
+	if err := binary.Read(reader, binary.BigEndian, &request.MessageSize); err != nil {
+		fmt.Println("Error reading MessageSize:", err)
+		return
+	}
+	if err := binary.Read(reader, binary.BigEndian, &request.RequestApiKey); err != nil {
+		fmt.Println("Error reading RequestApiKey:", err)
+		return
+	}
+	if err := binary.Read(reader, binary.BigEndian, &request.RequestApiVersion); err != nil {
+		fmt.Println("Error reading RequestApiVersion:", err)
+		return
+	}
+	if err := binary.Read(reader, binary.BigEndian, &request.CorrelationID); err != nil {
+		fmt.Println("Error reading CorrelationID:", err)
+		return
 	}
 
-	// If RequestApiVersion is not in 0 to 4, return UNSUPPORTED_VERSION
-	if request.RequestApiVersion < 0 || request.RequestApiVersion >= 4 || request.RequestApiVersion > MAX_API_VERSION {
+	// Prepare response with supported API key
+	responseKeys := []ApiKeyVersion{
+		{
+			ApiKey:     18,
+			MinVersion: 0,
+			MaxVersion: 4,
+		},
+		{
+			ApiKey:     3,
+			MinVersion: 0,
+			MaxVersion: 4,
+		},
+		{
+			ApiKey:     5,
+			MinVersion: 0,
+			MaxVersion: 4,
+		},
+	}
+
+	response := ApiVersionsResponse{
+		CorrelationID:  request.CorrelationID,
+		ApiKeyCount:    int8(len(responseKeys) + 1),
+		ApiKeys:        responseKeys,
+		ThrottleTimeMs: 0,
+	}
+
+	// If version is out of desired range, set an error code
+	if request.RequestApiVersion < 0 || request.RequestApiVersion > 4 {
 		response.ErrorCode = 35
 	} else {
 		response.ErrorCode = 0
-		response.APIVersion = request.RequestApiVersion
 	}
 
-	// Serialize response
+	// Serialize the response
 	var responseBuf bytes.Buffer
-	binary.Write(&responseBuf, binary.BigEndian, response.MessageSize)
-	binary.Write(&responseBuf, binary.BigEndian, response.CorrelationID)
-	binary.Write(&responseBuf, binary.BigEndian, response.ErrorCode)
-	binary.Write(&responseBuf, binary.BigEndian, response.APIVersion)
 
-	// Send complete response
-	conn.Write(responseBuf.Bytes())
+	// 1) Correlation ID
+	binary.Write(&responseBuf, binary.BigEndian, response.CorrelationID)
+	// 2) Error code
+	binary.Write(&responseBuf, binary.BigEndian, response.ErrorCode)
+	// 3) ApiKeyCount
+	binary.Write(&responseBuf, binary.BigEndian, response.ApiKeyCount)
+
+	// 4) Api keys
+	for _, key := range response.ApiKeys {
+		binary.Write(&responseBuf, binary.BigEndian, key.ApiKey)
+		binary.Write(&responseBuf, binary.BigEndian, key.MinVersion)
+		binary.Write(&responseBuf, binary.BigEndian, key.MaxVersion)
+		// Add tag buffer after each api key
+		binary.Write(&responseBuf, binary.BigEndian, byte(0))
+	}
+
+	// 5) ThrottleTimeMs
+	binary.Write(&responseBuf, binary.BigEndian, response.ThrottleTimeMs)
+
+	// 6) Tag Buffer
+	binary.Write(&responseBuf, binary.BigEndian, byte(0))
+
+	// Total size of the response body
+	response.MessageSize = int32(responseBuf.Len())
+
+	// Prepend MessageSize to response
+	finalBuf := &bytes.Buffer{}
+	binary.Write(finalBuf, binary.BigEndian, response.MessageSize)
+
+	finalBuf.Write(responseBuf.Bytes())
+
+	// // Print byte count for each part
+	// fmt.Println("CorrelationID:", binary.Size(response.CorrelationID))
+	// fmt.Println("ErrorCode:", binary.Size(response.ErrorCode))
+	// fmt.Println("ApiKeyCount:", binary.Size(response.ApiKeyCount))
+	// fmt.Println("ApiKeys:", binary.Size(response.ApiKeys))
+	// fmt.Println("ThrottleTimeMs:", binary.Size(response.ThrottleTimeMs))
+
+	// // Total byte count
+	// fmt.Println("Total bytes:", finalBuf.Len())
+
+	conn.Write(finalBuf.Bytes())
 }
